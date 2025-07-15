da246d7b01495097eb50c8bf72e42824ed6c71eb
Void-ux <77840397+Void-ux@users.noreply.github.com>
2025-07-15 15:10:59 +0100

    fix: add BotT

--- src/ipc/worker/worker.py
+++ src/ipc/worker/worker.py
@@ -7,7 +7,9 @@ from typing import TYPE_CHECKING, Callable, Any
 from .mdwrkapi import MDWorker
 
 if TYPE_CHECKING:
-    from discord.ext.commands import Bot
+    from discord.ext.commands import Bot, AutoShardedBot
+    from discord import Client
+    BotT = Bot | AutoShardedBot | Client
 
 __all__ = ('route', 'IPC')
 log = logging.getLogger('ipc.worker')
@@ -22,7 +24,7 @@ def route(name: str | None = None):
     return decorator
 
 
-async def _worker(service_name: str, broker_ip: str, broker_port: int, bot: Bot, func: Callable, *, log_level=logging.INFO):
+async def _worker(service_name: str, broker_ip: str, broker_port: int, bot: BotT, func: Callable, *, log_level=logging.INFO):
     worker_ = MDWorker(service_name.encode(), broker_ip, broker_port, log_level=log_level)
     await worker_.connect_to_broker()
 
@@ -42,7 +44,7 @@ def log_errors(task: asyncio.Task):
 class IPC:
     ROUTES: dict[str, Any] = {}
 
-    def __init__(self, bot: Bot, *, broker_ip: str = '127.0.0.1', broker_port: int = 5555):
+    def __init__(self, bot: BotT, *, broker_ip: str = '127.0.0.1', broker_port: int = 5555):
         self.bot = bot
         self.ip = broker_ip
         self.port = broker_port
